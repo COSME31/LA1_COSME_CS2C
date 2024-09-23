18cf8259371f2b2c2b14f99ba2d4060ac6f895ff
COSME, HAROLD D. <haroldcosme27@gmail.com>
2024-09-23 22:49:40 +0800

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,9 @@
+let name = "Harold Dulay Cosme";
+let WhatDoYouWannaBecomeInYourLife = "Business Man";
+let gender = "male";
+let fbHandle = "Cosme31";
+
+console.log(name);
+console.log(WhatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(fbHandle);
