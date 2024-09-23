d9414f824ccf2726cc4cdd4d954520ceef95346a
COSME, HAROLD D. <haroldcosme27@gmail.com>
2024-09-23 22:53:29 +0800

    Add files via upload

---
+++ activity4.js
@@ -0,0 +1,20 @@
+let firstFavNumb = 01;
+let secondFavNumb = 27;
+
+let a = firstFavNumb > secondFavNumb;
+let b = firstFavNumb < secondFavNumb;
+let c = firstFavNumb >= secondFavNumb;
+let d = firstFavNumb <= secondFavNumb;
+let e = firstFavNumb === secondFavNumb;
+let f = firstFavNumb == secondFavNumb;
+let g = firstFavNumb !== secondFavNumb;
+let h = firstFavNumb != secondFavNumb;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
