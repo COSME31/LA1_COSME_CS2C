95989691a061058457f547f972192e26bdeb30bd
COSME, HAROLD D. <haroldcosme27@gmail.com>
2024-09-23 22:53:58 +0800

    Add files via upload

---
+++ activity5.js
@@ -0,0 +1,11 @@
+let FavActorFirstName = "Andrew Ford";
+let FavActorLastName = " Espiritu";
+let fullname = FavActorFirstName + FavActorLastName;
+let uppercase = fullname.toUpperCase();
+let message = "My favorite actor is" + uppercase + "his best show is silicon valley";
+
+console.log(FavActorFirstName);
+console.log(FavActorLastName);
+console.log(fullname);
+console.log(uppercase);
+console.log(message);
