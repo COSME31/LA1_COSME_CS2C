36e99216f79df4edac57f22d6126ac2bfe10266a
COSME, HAROLD D. <haroldcosme27@gmail.com>
2024-09-23 22:51:19 +0800

    Add files via upload

---
+++ activity2.js
@@ -0,0 +1,16 @@
+let firstFavNum = 01;
+let secondFavNum = 27;
+
+let a = firstFavNum + secondFavNum;
+let b = firstFavNum - secondFavNum;
+let c = firstFavNum * secondFavNum;
+let d = firstFavNum / secondFavNum;
+let e = firstFavNum % secondFavNum;
+let f = firstFavNum ** secondFavNum;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
